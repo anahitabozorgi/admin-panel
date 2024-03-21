e1445b91778fd549ee6976d2f288961cc34281c4
Anahita <anahita@Marcos-MacBook-Pro.local>
2024-03-21 11:56:24 +0400

    changing text

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Index = () => {
     return(
         <div>
-            <h1>Index page</h1>
+            <h1>Index v2 page</h1>
             <Link to={'/about'}>go to about page</Link>
         </div>
     )
